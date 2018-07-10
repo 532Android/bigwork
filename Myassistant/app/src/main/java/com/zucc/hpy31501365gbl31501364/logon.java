761ab0036600b4f673e79397ab704ed5e5570e2c
GBL <357216043@qq.com>
2018-07-10 14:14:07 +0800

    GBL

---
+++ Myassistant/app/src/main/java/com/zucc/hpy31501365gbl31501364/logon.java
@@ -0,0 +1,82 @@
+package com.zucc.hpy31501365gbl31501364;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * Created by L-Jere on 2018/7/10.
+ */
+
+public class logon extends AppCompatActivity {
+
+    private SharedPreferences pref;
+    private SharedPreferences.Editor editor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.logon);
+        Button Logon = (Button)findViewById(R.id.logon);
+        final Button Sign = (Button)findViewById(R.id.sign);
+        final EditText Username = (EditText)findViewById(R.id.username);
+        final EditText Password = (EditText)findViewById(R.id.password);
+        final CheckBox Remember = (CheckBox)findViewById(R.id.rember);
+        pref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        boolean isRemember = pref.getBoolean("remember", false);
+        if(isRemember){
+            String U = pref.getString("username", "");
+            String P = pref.getString("password", "");
+            Username.setText(U);
+            Password.setText(P);
+            Remember.setChecked(true);
+            System.out.println("Username:" + U);
+        }
+        Logon.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String U = Username.getText().toString();
+                String P = Password.getText().toString();
+                SharedPreferences pre = getSharedPreferences("data", MODE_PRIVATE);
+                // TODO 发请求到后台
+                if(U.equals(pre.getString("username", ""))&&P.equals(pre.getString("password", ""))){
+                    editor = pref.edit();
+                    if (Remember.isChecked()) {
+                        editor.putBoolean("remember", true);
+                    }
+                    else{
+                        editor.clear();
+                    }
+                    editor.commit();
+                    Intent intent = new Intent(logon.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"帐号或密码错误",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        Sign.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(logon.this,sign_up.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    // 帐号密码存入sharedPrefereces
+    // Todo
+
+    // post后台
+    // Todo
+}
---
+++ Myassistant/app/src/main/java/com/zucc/hpy31501365gbl31501364/sign_up.java
@@ -0,0 +1,63 @@
+package com.zucc.hpy31501365gbl31501364;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * Created by L-Jere on 2018/7/10.
+ */
+
+public class sign_up extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+        final EditText Name = (EditText)findViewById(R.id.name);
+        final EditText Username = (EditText)findViewById(R.id.username);
+        final EditText Password1 = (EditText)findViewById(R.id.password1);
+        final EditText Password2 = (EditText)findViewById(R.id.password2);
+        Button Sign = (Button) findViewById(R.id.sign);
+        Sign.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String N = Name.getText().toString();
+                String Un = Username.getText().toString();
+                String P1 = Password1.getText().toString();
+                String P2 = Password2.getText().toString();
+                if(N.length()==0){
+                    Toast.makeText(getApplicationContext(),"昵称不能为空",Toast.LENGTH_SHORT).show();
+                }
+                else if(Un.length()==0){
+                    Toast.makeText(getApplicationContext(),"帐号不能为空",Toast.LENGTH_SHORT).show();
+                }
+                else if(P1.length()==0){
+                    Toast.makeText(getApplicationContext(),"密码不能为空",Toast.LENGTH_SHORT).show();
+                }
+                else if(P2.length()==0){
+                    Toast.makeText(getApplicationContext(),"密码不能为空",Toast.LENGTH_SHORT).show();
+                }
+                else if(!P1.equals(P2)){
+                    Toast.makeText(getApplicationContext(),"两次密码不相等",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    SharedPreferences.Editor editor = getSharedPreferences("data", MODE_PRIVATE).edit();
+                    editor.putString("name", N);
+                    editor.putString("username",Un);
+                    editor.putString("password",P1);
+                    editor.commit();
+                    finish();
+                    Toast.makeText(getApplicationContext(),"注册成功",Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(sign_up.this,logon.class);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+}
