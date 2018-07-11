8bb8c2201b49d93625cbd54700c6defb394eebcf
GBL <357216043@qq.com>
2018-07-11 16:22:03 +0800

    GBL

---
+++ Myassistant/app/src/main/java/com/zucc/hpy31501365gbl31501364/AddEventActivity.java
@@ -0,0 +1,13 @@
+package com.zucc.hpy31501365gbl31501364;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_event);
+    }
+}
